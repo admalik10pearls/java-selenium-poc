ef126454ad0384ec19bf2a44915bec0b5f1af072
admalik10pearls <ahmad.amir@10pearls.com>
2026-01-30 18:15:48 +0500

    add browser log

--- src/main/java/framework/base/BaseTest.java
+++ src/main/java/framework/base/BaseTest.java
@@ -2,6 +2,7 @@ package framework.base;
 
 import framework.config.ConfigReader;
 import framework.driver.DriverFactory;
+import framework.utils.LoggerUtils;
 import framework.utils.ScreenshotUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,6 +21,8 @@ public abstract class BaseTest {
 
         driver = DriverFactory.createDriver(browser);
         driver.manage().window().maximize();
+        LoggerUtils.info("Running tests on browser: " + browser);
+
     }
 
     @AfterEach
